d7b256675fe48384663bb0d2020b9ea51afe8ea1
AdamCayuela <cayuelaadam@gmail.com>
2026-02-04 12:03:26 +0100

    Update User.java

--- app/src/main/java/com/example/demo/entity/User.java
+++ app/src/main/java/com/example/demo/entity/User.java
@@ -11,7 +11,7 @@ import java.util.Set;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //@GeneratedValue (strategy = GenerationType.UUID) pour générer un Unique & Universal ID
     private Long id;
 
     @Column(nullable = false, unique = true)
